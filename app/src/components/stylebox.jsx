a96162b6f10f1f69e7fd6dcf7b8797a7d1dab8a3
JoeSteeb <josteeb@gmail.com>
2025-03-07 18:25:01 -0800

    Updated taskbar styling

--- app/src/components/stylebox.jsx
+++ app/src/components/stylebox.jsx
@@ -1,23 +1,28 @@
 // components/Taskbar.jsx
-import React from 'react';
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 export const Stylebox = ({ content }) => {
   return (
-    <div className='taskbar'>
-      <div className='inline'>
-        <div className='corner-tl'/>
-        <div className='corner-tr'/>
-      </div>
-        
-      <div className='background1'>
-          {content}
+    <div className="taskbar">
+      <div className="inline">
+        <div className="corner-tl" />
+        <div className="middle middle-top" />
+        <div className="corner-tr" />
       </div>
 
-      <div className='inline'>
-          <div className='corner-bl'/>
-          <div className='middle'></div>
-          <div className='corner-br'/>
+      <div className="background1">
+        <div className="inline v-fill">
+          <div className="side" />
+          {content}
+          <div className="side" />
+        </div>
+      </div>
+
+      <div className="inline">
+        <div className="corner-bl" />
+        <div className="middle middle-bottom" />
+        <div className="corner-br" />
       </div>
     </div>
   );
