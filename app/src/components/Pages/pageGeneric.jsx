2c34cd8d00172555e63d9aab7be0e3ee6a9c7727
JoeSteeb <JoeSteeb>
2025-03-09 17:05:57 -0700

    Added loading circle.

--- app/src/components/Pages/pageGeneric.jsx
+++ app/src/components/Pages/pageGeneric.jsx
@@ -1,7 +1,21 @@
-import "./page.css"
-export const PageGeneric = ({title, content}) => {
-    return <div className="page-wrapper">
-        <div className="page-title">{title}</div>
-        <div className="page-content">{content}</div>
+import React, { useState, useEffect } from "react";
+import "./page.css";
+
+export const PageGeneric = ({ title, content }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (content) {
+      setLoading(false);
+    }
+  }, [content]);
+
+  return (
+    <div className="page-wrapper">
+      <div className="page-title">{title}</div>
+      <div className="page-content">
+        {loading ? <div className="loading-circle"></div> : content}
+      </div>
     </div>
-}
+  );
+};
